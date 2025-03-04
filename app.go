5c9850c46f0bb9fb13174e90e3471e6917dfc358
Richard Tingstad <richard.tingstad@gmail.com>
2025-03-04 14:53:14 +0100

    ws poc

--- app.go
+++ app.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "embed"
-    "html/template"
-    "log"
-    "net/http"
-    "os"
+	"embed"
+	"github.com/gorilla/websocket"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 )
 
 //go:embed templates/*
@@ -13,22 +14,50 @@ var resources embed.FS
 
 var t = template.Must(template.ParseFS(resources, "templates/*"))
 
+var upgrader = websocket.Upgrader{} // use default options
+
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 
-    }
+	}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        data := map[string]string{
-            "Region": os.Getenv("FLY_REGION"),
-        }
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"Region": os.Getenv("FLY_REGION"),
+		}
 
-        t.ExecuteTemplate(w, "index.html.tmpl", data)
-    })
+		t.ExecuteTemplate(w, "index.html.tmpl", data)
+	})
+	http.HandleFunc("/ws", ws)
 
-    log.Println("listening on", port)
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Println("listening on", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
+func ws(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer func() {
+		if err := c.Close(); err != nil {
+			log.Println("warn: closing ws", err)
+		}
+	}()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
